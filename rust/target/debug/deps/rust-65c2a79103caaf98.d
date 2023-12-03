161f02b742daed73ba7a571c796db7990fc14e16
Martin Helmer <martin.helmer@ymq000300.home>
2023-12-03 08:49:19 -0500

    rmeovere rust target

--- rust/target/debug/deps/rust-65c2a79103caaf98.d
+++
@@ -1,5 +0,0 @@
-/Users/martin.helmer/FromNew/govert/Git/AOC2023/rust/target/debug/deps/rust-65c2a79103caaf98: src/main.rs
-
-/Users/martin.helmer/FromNew/govert/Git/AOC2023/rust/target/debug/deps/rust-65c2a79103caaf98.d: src/main.rs
-
-src/main.rs:
--- rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/Users/martin.helmer/FromNew/govert/Git/AOC2023/rust/target/debug/rust: /Users/martin.helmer/FromNew/govert/Git/AOC2023/rust/src/main.rs
